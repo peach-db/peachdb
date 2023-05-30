0e40764461f84176da200d202fe2b5ad955569f3
Vatsal <vatsal@themetavoice.xyz>
2023-05-30 15:30:23 +0000

    update setup.py to include more info

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 requirements = []
@@ -6,10 +8,22 @@ requirements = []
 with open("requirements.txt", "r") as f:
     requirements.extend(f.read().splitlines())
 
+# read the contents README
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
-    name="PeachDB",
-    version="0.0.1",
+    name="peachdb",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=requirements,
     dependency_links=["https://download.pytorch.org/whl/cu113"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Apache License 2.0",
+    project_urls={
+        "Source on GitHub": "https://github.com/peach-db/peachdb",
+        "Documentation": "https://github.com/peach-db/peachdb/blob/master/README.md",
+    },
+    python_requires=">=3.10",
 )
