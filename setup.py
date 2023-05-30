b154d6b73416e46e028ca86c6b92677cf36a668c
Vatsal <vatsal@themetavoice.xyz>
2023-05-30 16:39:21 +0000

    add MANIFEST.in so setup.py has access to requirements

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="peachdb",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(),
     install_requires=requirements,
     dependency_links=["https://download.pytorch.org/whl/cu113"],
