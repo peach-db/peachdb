becc6c5d2a89c372b1b65f2f6455320d2baa3e19
siddharth sharma <siddharthsharma0508@gmail.com>
2023-06-06 19:38:11 +0100

    bump version: 0.0.16 -> 0.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="peachdb",
-    version="0.0.16",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=requirements,
     dependency_links=["https://download.pytorch.org/whl/cu113"],
